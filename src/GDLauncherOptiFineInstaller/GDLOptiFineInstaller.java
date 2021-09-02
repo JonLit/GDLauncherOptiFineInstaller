5f85a09b2ac703e3be196a7224f22868e24b38c8
JonLit <max-huntermann@web.de>
2021-09-02 20:52:03 +0200

    optimize Imports

--- src/GDLauncherOptiFineInstaller/GDLOptiFineInstaller.java
+++ src/GDLauncherOptiFineInstaller/GDLOptiFineInstaller.java
@@ -2,8 +2,6 @@ package GDLauncherOptiFineInstaller;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 
 public class GDLOptiFineInstaller {
